0d238140426b81aea703f2a74b52e83e475547fa
Radek Molenda <radek.molenda@gmail.com>
2018-03-16 14:11:22 +0100

    Dummy

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ var fs = require('fs');
 var path = require('path');
 
 exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
   var result = {
     statusCode: 200,
-    body: contents.toString(),
+    body: "hello world",
     headers: {'content-type': 'text/html'}
   };
 
